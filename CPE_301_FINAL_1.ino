cb2dc9e7c15155a30c892d4e8729a4dae96191c1
2012-Pai-Raymond <rpai@nevada.unr.edu>
2022-05-04 20:32:50 -0700

    Add files via upload

--- CPE_301_FINAL_1.ino
+++ CPE_301_FINAL_1.ino
@@ -5,36 +5,16 @@
 #include <Wire.h>
 #include <ds3231.h>
 
+//Setting all ports
 volatile unsigned char* ddr_a  = (unsigned char*) 0x21;
 volatile unsigned char* port_a = (unsigned char*) 0x22;
 volatile unsigned char* pin_a  = (unsigned char*) 0x20;
 volatile unsigned char* ddr_b  = (unsigned char*) 0x24;
 volatile unsigned char* port_b = (unsigned char*) 0x25;
 volatile unsigned char* pin_b  = (unsigned char*) 0x23;
-volatile unsigned char* ddr_c  = (unsigned char*) 0x27;
-volatile unsigned char* port_c = (unsigned char*) 0x28;
-volatile unsigned char* pin_c = (unsigned char*) 0x26;
-volatile unsigned char* ddr_d  = (unsigned char*) 0x2A;
-volatile unsigned char* port_d = (unsigned char*) 0x2B;
-volatile unsigned char* pin_d = (unsigned char*) 0x29;
 volatile unsigned char* ddr_e  = (unsigned char*) 0x2D;
 volatile unsigned char* port_e = (unsigned char*) 0x2E;
 volatile unsigned char* pin_e = (unsigned char*) 0x2C;
-volatile unsigned char* ddr_f  = (unsigned char*) 0x30;
-volatile unsigned char* port_f = (unsigned char*) 0x31;
-volatile unsigned char* pin_f = (unsigned char*) 0x2F;
-volatile unsigned char* ddr_g  = (unsigned char*) 0x33;
-volatile unsigned char* port_g = (unsigned char*) 0x34;
-volatile unsigned char* pin_g = (unsigned char*) 0x32;
-volatile unsigned char* ddr_h  = (unsigned char*) 0x101;
-volatile unsigned char* port_h = (unsigned char*) 0x102;
-volatile unsigned char* pin_h = (unsigned char*) 0x100;
-volatile unsigned char* ddr_j  = (unsigned char*) 0x104;
-volatile unsigned char* port_j = (unsigned char*) 0x105;
-volatile unsigned char* pin_j = (unsigned char*) 0x103;
-volatile unsigned char* ddr_k  = (unsigned char*) 0x107;
-volatile unsigned char* port_k = (unsigned char*) 0x108;
-volatile unsigned char* pin_k = (unsigned char*) 0x106;
 volatile unsigned char* ddr_l  = (unsigned char*) 0x10A;
 volatile unsigned char* port_l = (unsigned char*) 0x10B;
 volatile unsigned char* pin_l = (unsigned char*) 0x109;
@@ -44,7 +24,7 @@ volatile unsigned char* pin_l = (unsigned char*) 0x109;
 #define SIGNAL_PIN 0 // water sensor analog pin
 #define THRESHOLD 20 // water sensor threshold limit
 #define STEPS 32 // stepper motor steps
-#define TEMPHOLD 23.0 // Temperatrue Threshold
+#define TEMPHOLD 22.0 // Temperatrue Threshold
 
 
 //water sensor var
@@ -55,50 +35,63 @@ const int button = 6; // direction control button is connected to Arduino pin 6
 const int pot = A0; // speed control potentiometer is connected to analog pin 0
 
 //LCD var
-const int RS = 12, EN = 7, D4 = 2, D5 = 3, D6 = 4, D7 = 5;
+//Setting LCD with correct pins
+const int RS = 12, EN = 7, D4 = 2, D5 = 30, D6 = 4, D7 = 5;
 
 //Humidity and Temperature var
 dht DHT;
 
-//Real-Time Clock var
+//Real-Time Clock struct var
 struct ts t;
 
-/*const int buttonStart = 52;
-const int buttonStop = 50;
-const int buttonReset = 48;*/
-
-const int buttonStart = 1;
+//Button Setup
+const int buttonStart = 5;
 const int buttonStop = 3;
 const int buttonReset = 1;
+const int buttonDC = 3;
 
-const int ledPinG = 0;
+//LED Setup
+const int ledPinG = 6;
 const int ledPinY = 2;
 const int ledPinB = 4;
-const int ledPinR = 6;
-const int dcMotor = 0;
+const int ledPinR = 0;
 
+//LCD and Stepper Motor object constructors 
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 Stepper stepper(STEPS, 8, 10, 9, 11);
 
-boolean Idle = false, Running = true, Error = false, Disabled = false;
+//Default values of each state
+boolean Idle = false, Running = true, Error = false;
+
+//Default values for Steppe
 int direction_ = 1, speed_ = 1000;
+
 volatile int state = 0;
+volatile int lcdState = 0;
 volatile bool stateSys = false;
+
+volatile bool currentState = 0;
+volatile bool previousState = 1;
+
 void setup(){
  Serial.begin(9600);
- setInput(ddr_b, buttonStart);
- enablePullup(port_b, buttonStart);
+
+ //Setting Intputs
+ setInput(ddr_e, buttonStart);
+ enablePullup(ddr_e, buttonStart);
  setInput(ddr_b, buttonStop);
- enablePullup(port_b, buttonStop);
+ enablePullup(ddr_b, buttonStop);
  setInput(ddr_l, buttonReset);
- enablePullup(port_l, buttonReset);
- 
+ enablePullup(ddr_l, buttonReset);
+
+ //Setting Outputs
  setOutput(ddr_a, ledPinG);
  setOutput(ddr_a, ledPinY);
  setOutput(ddr_a, ledPinB);
  setOutput(ddr_a, ledPinR);
- setOutput(ddr_b, dcMotor);
+ setOutput(ddr_l, buttonDC);
  
+ //LED setup values
  write_port(port_a, ledPinG, 0);
  write_port(port_a, ledPinY, 0);
  write_port(port_a, ledPinB, 0);
@@ -106,6 +99,7 @@ void setup(){
  
  //Stepper Motor Setup
  pinMode(button, INPUT_PULLUP);
+ 
  //Water Sensor Setup
  adc_init();
  setOutput(ddr_b, POWER_PIN);
@@ -119,111 +113,145 @@ void setup(){
  //Real Time Clock Setup
  Wire.begin();
  DS3231_init(DS3231_CONTROL_INTCN);
-  /*----------------------------------------------------------------------------
-  In order to synchronise your clock module, insert timetable values below !
-  ----------------------------------------------------------------------------*/
-  t.sec=0;
  
+ //Inserting starting clock value;
+ t.sec = 0;
  DS3231_set(t); 
-  
- //PCICR  |= 1 << PCIE0;
- //PCMSK0 |= 1 << PCINT1;
+
+ //Setting and Enabling ISR 
+ //PCICR  |= 0x1 << PCIE0;
+ //PCMSK0 |= 0x1 << PCINT1;
+ EICRB |= 1 << ISC51;
+ EICRB &= ~(1 << ISC50);
+ EIMSK |= 1 << INT5;
+
+ sei();
  
 }
-/*ISR(PCINT1_vect){
-}*/
 void loop(){
-   if(state == 0 && digital_read(pin_b, buttonStart) == HIGH){
+   /*if(state == 0 && digital_read(pin_b, buttonStart) == HIGH){
       state = 1;
       stateSys =! stateSys;
-      lcd.clear();
    }
-   if(state == 1 && digital_read(pin_b, buttonStart) == LOW){
-      state = 0;
-      stateSys = false;
-      lcd.clear();
-   }
-   if(!stateSys){
-      write_port(port_b,POWER_PIN, 1);              
+   if(state == 1 && digital_read(pin_b, buttonStop) == HIGH){
+        state = 0;
+        stateSys = false;
+   }*/
+   while(previousState == false && currentState == true){
+      write_port(port_a, ledPinY, 0);
+      if(lcdState == 1){
+        lcd.clear();
+        lcdState = 0;
+      }
+      
+      //Reads from Water Sensor
+      write_port(port_b,POWER_PIN, 1);
+      //Serial.println(value);              
       value = adc_read(SIGNAL_PIN);
       write_port(port_b,POWER_PIN, 0);
+      
+      //Runs Running State
       if(Running == true){
-        write_port(port_b, dcMotor, 1);
+        write_port(port_l, buttonDC, 1);
         lcd.setCursor(0, 0);
         lcd.print("R");
         displayLCD();
-        //write_port(port_a, ledPinR, 1);
+        write_port(port_a, ledPinB, 1);
         if(DHT.temperature <= TEMPHOLD){
           lcd.clear();
-          write_port(port_b, dcMotor, 0);
           Idle = true;
           Running = false;
-          //write_port(port_a, ledPinB, 0);
+          write_port(port_a, ledPinB, 0);
+          write_port(port_l, buttonDC, 0);
           serialTime("Running -> Idle");
         }
         else if(value < THRESHOLD){
           lcd.clear();
-          write_port(port_b, dcMotor, 0);
           Error = true;
           Running = false;
-          //write_port(port_a, ledPinB, 0);
+          write_port(port_a, ledPinB, 0);
+          write_port(port_l, buttonDC, 0);
           serialTime("Running -> Error");
         }
       }
+      //Runs Idle State
       if(Idle == true){
         lcd.setCursor(0, 0);
         lcd.print("I");
         displayLCD();
-        //write_port(port_a, ledPinG, 1);
+        write_port(port_a, ledPinG, 1);
         if(DHT.temperature > TEMPHOLD){
           lcd.clear();
           Running = true;
           Idle = false;
-          //write_port(port_a, ledPinG, 0);
+          write_port(port_a, ledPinG, 0);
           serialTime("Idle -> Running");
         }
         else if(value <= THRESHOLD){
           lcd.clear();
           Error = true;
           Idle = false;
-          //write_port(port_a, ledPinG, 0);
+          write_port(port_a, ledPinG, 0);
           serialTime("Idle -> Error");
         }
       }
+      //Runs Error State;
       if(Error == true){
-        //write_port(port_a, ledPinR, 1);
+        write_port(port_a, ledPinR, 1);
         ErrorLCD();
-        if(value > THRESHOLD){
+        if(value >= THRESHOLD){
             if(digital_read(pin_l, buttonReset) == HIGH){
               lcd.clear();
               Idle = true;
               Error = false;
-              //write_port(port_a, ledPinR, 0);
+              write_port(port_a, ledPinR, 0);
               serialTime("Error -> Idle");
           }
         }
      }
   }
-  else{
+  while(previousState == true && currentState == false){
+      write_port(port_a, ledPinB, 0);
+      write_port(port_a, ledPinG, 0);
+      write_port(port_a, ledPinR, 0);
+      write_port(port_a, ledPinY, 1);
+      write_port(port_l, buttonDC, 0);
+      if(lcdState == 0){
+        lcd.clear();
+      }
       lcd.setCursor(0, 0);
       lcd.print("Disabled");
+      lcdState = 1;
   }
 }
 void motorAct(int speed_){
   if (digitalRead(button) == 0 && Error == false) { // if button is pressed
       if (stepDebounce()){  // debounce button signal
          direction_ *= -1;  // reverse direction variable
+         if(direction_ == -1){
+             Serial.println("Motor Direction: Right");
+          }
+          else{
+             Serial.println("Motor Direction: Left");
+         }
          while (stepDebounce());  // wait for button release
       }
   }
   stepper.setSpeed(speed_);
   stepper.step(direction_); 
 }
+//Enable ISR to pin 52
+ISR(INT5_vect){
+  previousState =! previousState;
+  currentState =! currentState;
+}
+//Motor Running 
 void motorRun(){
   for(int i = 0; i < 20; i++){
     motorAct(speed_);
   }
 }
+//Prints Serial Time and state change
 void serialTime(String state){
   DS3231_get(&t);
   Serial.print(state);
@@ -231,6 +259,8 @@ void serialTime(String state){
   Serial.print("Seconds: ");
   Serial.println(t.sec);
 }
+
+//Special LCD for Error state and runs motor
 void ErrorLCD(){
   motorRun();
   lcd.setCursor(0, 0);
@@ -238,6 +268,8 @@ void ErrorLCD(){
   lcd.setCursor(0, 1);
   lcd.print("Water too low");
 }
+
+//Displays LCD values and runs motor
 void displayLCD(){
   motorRun();
   DHT.read11(dht_apin);
@@ -249,21 +281,26 @@ void displayLCD(){
   lcd.print(DHT.temperature);
 
 }
+//Reads value of pin using assigned port and pin number
 boolean digital_read(volatile unsigned char* pin, volatile unsigned char pin_num){
     if((*pin & (0x01 << pin_num)) != 0){
       return HIGH;
     }
     return LOW;
 }
+//Sets output of pin
 void setOutput(volatile unsigned char* ddr, unsigned char pin_num){
     *ddr |= 0x01 << pin_num;
 }
+//sests input of pin
 void setInput(volatile unsigned char* ddr, unsigned char pin_num){
     *ddr &= ~(0x01 << pin_num);
 }
+//Enables Pullup
 void enablePullup(volatile unsigned char *port, volatile unsigned char pin_num){
     *port |= 0x01 << pin_num;
 }
+//Writes to pin with state value 0 or 1
 void write_port(volatile unsigned char* port, unsigned char pin_num, unsigned char state){
   if(state == 0)
   {
@@ -274,6 +311,7 @@ void write_port(volatile unsigned char* port, unsigned char pin_num, unsigned ch
     *port |= 0x01 << pin_num;
   }
 }
+//Debounce method for Stepper Motor
 bool stepDebounce()
 {
   byte count = 0;
@@ -285,6 +323,7 @@ bool stepDebounce()
   if(count > 2)  return 1;
   else           return 0;
 }
+//enables ADC
 void adc_init()
 {
   // set up the A register
@@ -303,6 +342,7 @@ void adc_init()
   ADMUX  &= ~(1<<5); // clear bit 5 to 0 for right adjust result
   ADMUX  &= (0b11100000); // clear bit 4-0 to 0 to reset the channel and gain bits
 }
+//Reads from ADC
 unsigned int adc_read(unsigned char adc_channel_num){
   // reset the channel and gain bits
   ADMUX  &= (0b11100000);
